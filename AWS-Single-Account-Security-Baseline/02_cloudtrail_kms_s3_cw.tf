c025cf779e8378da52b47ea032534c4a53e38800
Raja Renga <rajagopal.rengarajan@gmail.com>
2023-03-30 06:06:45 +0000

    cloudtrail - update

--- AWS-Single-Account-Security-Baseline/02_cloudtrail_kms_s3_cw.tf
+++ AWS-Single-Account-Security-Baseline/02_cloudtrail_kms_s3_cw.tf
@@ -1,106 +1,123 @@
 //aws terraform request. give me breakdown of task to create cloudtrail with cmk, s3 and cloudwatch log group//
 
 
-//1. AWS terraform request. Create "akm-enterprises-cloudtrail-eu" S3 bucket, https enforced, KMS CMK for the cloudtrail, s3 lifecycle policy of moving object to IA tier after 30 days, to galicer after 90 days and expiry object after 365 days.//
+//1. AWS terraform request. Create "akm-enterprises-cloudtrail-eu" S3 bucket, https enforced, KMS CMK key "aws_kms_key.akm-enterprises-cloudtrail.arn" s3 lifecycle policy of moving object to IA tier after 30 days, to galicer after 90 days and expiry object after 365 days.//
 
 
-resource "aws_s3_bucket" "akm-enterprises-cloudtrail-eu" {
-  bucket = "akm-enterprises-cloudtrail-eu"
-  acl    = "private"
+resource "aws_iam_role" "akm-enterprises-cloudtrail-s3" {
+  name = "akm-enterprises-cloudtrail-s3"
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-        kms_master_key_id = "arn:aws:kms:eu-west-1:&lt;account-id&gt;:key/&lt;key-id&gt;"
-      }
-    }
-  }
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    id      = "move-to-ia-after-30-days"
-    enabled = true
-
-    prefix  = ""
-    tags    = {}
-
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-  }
-
-  lifecycle_rule {
-    id      = "move-to-glacier-after-90-days"
-    enabled = true
-
-    prefix  = ""
-    tags    = {}
-
-    transition {
-      days          = 90
-      storage_class = "GLACIER"
-    }
-  }
-
-  lifecycle_rule {
-    id      = "expire-after-365-days"
-    enabled = true
-
-    prefix  = ""
-    tags    = {}
-
-    expiration {
-      days = 365
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "akm-enterprises-cloudtrail-eu" {
-  bucket = aws_s3_bucket.akm-enterprises-cloudtrail-eu.id
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_policy" "akm-enterprises-cloudtrail-eu" {
-  bucket = aws_s3_bucket.akm-enterprises-cloudtrail-eu.id
-
-  policy = &lt;&lt;POLICY
+  assume_role_policy = <<-EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "PublicReadGetObject",
-      "Effect": "Deny",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::akm-enterprises-cloudtrail-eu/*",
-      "Condition": {
-        "StringNotEquals": {
-          "aws:SecureTransport": "https"
-        }
-      }
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
     }
   ]
 }
-POLICY
+EOF
 }
 
-//2. Create an IAM role with the necessary permissions to allow CloudTrail to write logs to the S3 bucket.
+resource "aws_iam_policy" "akm-enterprises-cloudtrail-s3" {
+  name = "akm-enterprises-cloudtrail-s3"
+
+  policy = <<-EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::akm-enterprises-cloudtrail-eu/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "akm-enterprises-cloudtrail-s3" {
+  role       = aws_iam_role.akm-enterprises-cloudtrail-s3.name
+  policy_arn = aws_iam_policy.akm-enterprises-cloudtrail-s3.arn
+}
 
 //3. Create a CloudTrail trail with the S3 bucket as the destination.
 
-//4. Create a CloudWatch Logs log group to store CloudTrail logs.
+//4. terraform-aws-Create a CloudWatch Logs log group called "akm-enterprises-cloudtrail" with retention of 365 days to store CloudTrail logs.
 
-//5. Create an IAM role with the necessary permissions to allow CloudTrail to write logs to the CloudWatch Logs log group.
 
-//6. Create a KMS key to encrypt the CloudTrail logs.
+resource "aws_cloudwatch_log_group" "akm-enterprises-cloudtrail" {
+  name              = "akm-enterprises-cloudtrail"
+  retention_in_days = 365
+}
+
+//5. terraform-aws Create an IAM role with the necessary permissions to allow CloudTrail to write logs to the CloudWatch Logs log group named "akm-enterprises-cloudtrail".
+
+
+resource "aws_iam_role" "cloudtrail_role" {
+  name = "akm-enterprises-cloudtrail-role"
+  assume_role_policy = <<-EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "cloudtrail_policy" {
+  name = "akm-enterprises-cloudtrail-policy"
+  policy = <<-EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": [
+        "arn:aws:logs:*:*:log-group:akm-enterprises-cloudtrail:*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "cloudtrail_policy_attachment" {
+  role       = "${aws_iam_role.cloudtrail_role.name}"
+  policy_arn = "${aws_iam_policy.cloudtrail_policy.arn}"
+  }
+  
+//6. Terraform aws script request. Create a KMS key name "akm-enterprises-cloudtrail", enable key reotation for CloudTrail logs.
+
+
+resource "aws_kms_key" "akm-enterprises-cloudtrail" {
+  description             = "KMS key for CloudTrail logs"
+  enable_key_rotation    = true
+  deletion_window_in_days = 30
+}
+
 
 //7. Update the CloudTrail trail to use the KMS key for encryption.
 
