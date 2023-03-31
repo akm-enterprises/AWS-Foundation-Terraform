6f64a11b140ee00cdff99af566643a9525a2799b
mekrish <102891286+raj-rengarajan@users.noreply.github.com>
2023-03-30 20:03:39 -0500

    aws config

---
+++ 03_aws_config.tf
@@ -0,0 +1,77 @@
+resource "aws_s3_bucket" "akm_enterprises_configlog_eu" {
+  bucket = "akm-enterprises-configlog-eu"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+data "aws_iam_policy_document" "akm_enterprises_configlog_eu_bucket_policy" {
+  statement {
+    actions   = ["s3:GetBucketAcl", "s3:PutObject"]
+    resources = [
+      aws_s3_bucket.akm_enterprises_configlog_eu.arn,
+      "${aws_s3_bucket.akm_enterprises_configlog_eu.arn}/*"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "akm_enterprises_configlog_eu_bucket_policy" {
+  bucket = aws_s3_bucket.akm_enterprises_configlog_eu.id
+  policy = data.aws_iam_policy_document.akm_enterprises_configlog_eu_bucket_policy.json
+}
+
+resource "aws_config_configuration_recorder" "config_recorder" {
+  name     = "default"
+  role_arn = aws_iam_role.config_role.arn
+
+  recording_group {
+    all_supported                 = true
+    include_global_resource_types = true
+  }
+}
+
+resource "aws_config_delivery_channel" "config_channel" {
+  name           = "default"
+  s3_bucket_name = aws_s3_bucket.akm_enterprises_configlog_eu.bucket
+  depends_on     = [aws_config_configuration_recorder.config_recorder]
+}
+
+resource "aws_iam_role" "config_role" {
+  name = "aws-config-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "config.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "config_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+  role       = aws_iam_role.config_role.name
+}
+
+resource "aws_config_configuration_recorder_status" "config_recorder_status" {
+  name       = aws_config_configuration_recorder.config_recorder.name
+  is_enabled = true
+}
