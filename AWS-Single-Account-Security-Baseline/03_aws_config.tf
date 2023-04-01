0408e3eab9a642d61d78111694e8567d1f21a539
mekrish <102891286+raj-rengarajan@users.noreply.github.com>
2023-04-01 16:52:39 -0500

    block public access

--- AWS-Single-Account-Security-Baseline/03_aws_config.tf
+++ AWS-Single-Account-Security-Baseline/03_aws_config.tf
@@ -12,6 +12,13 @@ resource "aws_s3_bucket" "akm_enterprises_configlog_eu" {
       }
     }
   }
+  # Block public access settings
+  public_access_block_configuration {
+    block_public_acls   = true
+    block_public_policy = true
+    ignore_public_acls  = true
+    restrict_public_buckets = true
+  }
 }
 
 data "aws_iam_policy_document" "akm_enterprises_configlog_eu_bucket_policy" {
