723ee70869d0c634c9e7b49bb7e363c4e87ad6b3
mekrish <102891286+raj-rengarajan@users.noreply.github.com>
2023-04-01 17:18:08 -0500

    new s3 bucket

---
+++ AWS-Single-Account-Security-Baseline/s3-tf-bucket.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "akm-enterprises-terraform-state-eu"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "s3:*"
+        Effect = "Deny"
+        Principal = "*"
+        Resource = "arn:aws:s3:::akm-enterprises-terraform-state-eu/*"
+        Condition = {
+          Bool = {
+            "aws:SecureTransport": "false"
+          }
+        }
+      }
+    ]
+  })
+}
+
+output "s3_bucket_arn" {
+  value = module.s3_bucket.s3_bucket_arn
+}
+
+output "s3_bucket_domain_name" {
+  value = module.s3_bucket.s3_bucket_domain_name
+}
